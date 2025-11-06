120cb2ffb22fb76d9c295113b6e0b55139cc3b15
Kosbys <kosmicabysslord@protonmail.com>
2025-11-06 17:54:10 +0200

    Add the basic grid

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Grid } from "./components/Grid";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <Grid />
     </div>
   );
 }
