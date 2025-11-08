deac183ad079f949c6e98a9dd8acde4a05a9dd55
Kosbys <kosmicabysslord@protonmail.com>
2025-11-08 11:13:02 +0200

    Format date view

--- front/src/components/WeekGrid.tsx
+++ front/src/components/WeekGrid.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { getSundayDate, getWeekDates } from "../utils/date";
 import { Button } from "./ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
 const WEEK_MILISECONDS = 86400000 * 7;
 
@@ -32,16 +40,34 @@ function WeekGrid() {
 
   return (
     <>
-      <Button type="button" onClick={previousWeek}>
-        Previous
-      </Button>
-      <Button type="button" onClick={nextWeek}>
-        Next
-      </Button>
-      <div className="grid grid-cols-5 gap-4 text-center">
-        {[...weekDates.values()].map((day) => (
-          <div key={crypto.randomUUID()}>{day}</div>
-        ))}
+      <div>
+        {" "}
+        <Button type="button" onClick={previousWeek}>
+          Previous
+        </Button>
+        <Button type="button" onClick={nextWeek}>
+          Next
+        </Button>
+      </div>
+      <div className="w-full" dir="rtl">
+        <Table className="w-full text-center border rounded-lg table-fixed">
+          <TableHeader>
+            <TableRow>
+              {[...weekDates.values()].map((day) => (
+                <TableHead
+                  key={day.date}
+                  className="text-center py-2 text-xs sm:text-sm w-1/5 wrap-break-word whitespace-normal"
+                >
+                  <div className="flex flex-col">
+                    <span>{day.day}</span>
+                    <span>{day.date}</span>
+                  </div>
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody></TableBody>
+        </Table>
       </div>
     </>
   );
--- front/src/utils/date.ts
+++ front/src/utils/date.ts
@@ -1,8 +1,6 @@
-const OPTIONS: Intl.DateTimeFormatOptions = {
-	weekday: "long",
-	month: "long",
-	year: "numeric",
-	day: "numeric",
+type FormattedDate = {
+	day: string;
+	date: string;
 };
 
 // 5 days in Sunday -> Thursday
@@ -12,15 +10,16 @@ function getSundayDate(today: Date, offset: number) {
 	return new Date(today.setDate(today.getDate() - offset));
 }
 
-function getWeekDates(sunday: Date): Map<number, string> {
+function getWeekDates(sunday: Date): Map<number, FormattedDate> {
 	const weekDates = new Map();
 
 	const currentDay = new Date(sunday);
 
 	for (let i = 0; i < WEEK_RANGE; i++) {
-		const dateString = currentDay.toLocaleDateString("he-IL", OPTIONS);
+		const day = currentDay.toLocaleString("he-IL", { weekday: "long" });
+		const date = currentDay.toLocaleDateString("en-GB").toString();
 
-		weekDates.set(i, dateString);
+		weekDates.set(i, { day, date });
 
 		currentDay.setDate(currentDay.getDate() + 1);
 	}
