e5c8330e1733aef0dd9d2c902778c5fe506dcc27
Kosbys <kosmicabysslord@protonmail.com>
2025-11-10 17:39:18 +0200

    Add dropdown to station to select section, TODO: query it

--- front/src/components/station/AddStation.tsx
+++ front/src/components/station/AddStation.tsx
@@ -11,8 +11,18 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const formSchema = z.object({
+  section: z.string(),
   computer: z.boolean(),
   monitor: z.boolean(),
 });
@@ -23,6 +33,7 @@ function AddStation() {
     defaultValues: {
       computer: false,
       monitor: false,
+      section: "",
     },
   });
 
@@ -37,6 +48,23 @@ function AddStation() {
         className="space-y-8 border-2 p-6"
       >
         <div className="flex flex-col items-start gap-12">
+          <FormField
+            control={form.control}
+            name="monitor"
+            render={({ field }) => (
+              <FormItem className="flex flex-row-reverse">
+                <FormLabel className="text-lg">מסך</FormLabel>
+                <FormControl>
+                  <Checkbox
+                    className="w-6 h-6"
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="computer"
@@ -56,16 +84,25 @@ function AddStation() {
           />
           <FormField
             control={form.control}
-            name="monitor"
+            name="section"
             render={({ field }) => (
-              <FormItem className="flex flex-row-reverse">
-                <FormLabel className="text-lg">מסך</FormLabel>
+              <FormItem className="" dir="rtl">
                 <FormControl>
-                  <Checkbox
-                    className="w-6 h-6"
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                  />
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    dir="rtl"
+                  >
+                    <SelectTrigger className="w-36">
+                      <SelectValue placeholder="בחירת סקשן" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>סקשנים</SelectLabel>
+                        <SelectItem value="apple">שמש</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
                 </FormControl>
                 <FormMessage />
               </FormItem>
