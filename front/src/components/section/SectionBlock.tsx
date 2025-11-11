b6af9d8c60c111cc2a29a3ece4c637796ec452d2
Kosbys <kosmicabysslord@protonmail.com>
2025-11-11 22:18:05 +0200

    Add section and station deletion

--- front/src/components/section/SectionBlock.tsx
+++ front/src/components/section/SectionBlock.tsx
@@ -1,26 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteSection } from "@/api/section";
 import { useAuthStore } from "@/store/auth";
-import type { Section } from "@/types/types";
+import type { Section, Station } from "@/types/types";
 import { StationBlock } from "../station/StationBlock";
 import { Button } from "../ui/button";
 
 function SectionBlock({ section }: { section: Section }) {
+  const queryClient = useQueryClient();
   const user = useAuthStore((s) => s.user);
 
   const stations = section.stations;
+
+  const deleteSectionMutation = useMutation({
+    mutationFn: (id: number) => deleteSection(id.toString()),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["sections"] });
+    },
+  });
 
   return (
     <div className="flex flex-col">
       <div className="flex flex-row items-center justify-between pb-1">
         <span className="text-lg">{section.name}</span>
         {user?.role === "admin" && (
-          <Button variant="destructive" className="">
+          <Button
+            variant="destructive"
+            className=""
+            onClick={() => {
+              deleteSectionMutation.mutate(section.id);
+            }}
+          >
             מחק
           </Button>
         )}
       </div>
 
       <div className="grid grid-cols-2 gap-0.5">
-        {stations.map((station) => (
+        {stations.map((station: Station) => (
           <StationBlock key={station.id} station={station} />
         ))}
       </div>
--- front/src/components/station/StationBlock.tsx
+++ front/src/components/station/StationBlock.tsx
@@ -1,10 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteStation } from "@/api/station";
 import { useAuthStore } from "@/store/auth";
 import type { Station } from "@/types/types";
 import { Button } from "../ui/button";
 import { StationTypeIcon } from "./StationTypeIcon";
 
 function StationBlock({ station }: { station: Station }) {
+  const queryClient = useQueryClient();
   const user = useAuthStore((s) => s.user);
+
+  const deleteSectionMutation = useMutation({
+    mutationFn: (id: number) => deleteStation(id.toString()),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["sections"] });
+    },
+  });
 
   return (
     <div className="group w-32 h-32 border-2 flex flex-col p-2 gap-1">
@@ -14,11 +24,13 @@ function StationBlock({ station }: { station: Station }) {
         <span>{station.type}</span>
       </span>
 
-      {/* should delete */}
       {user?.role === "admin" && (
         <Button
           variant="destructive"
           className="mt-auto self-end opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-black"
+          onClick={() => {
+            deleteSectionMutation.mutate(station.id);
+          }}
         >
           מחק
         </Button>
