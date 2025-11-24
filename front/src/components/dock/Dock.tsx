abd12798c47d4b3f4a18491b43e12c73bc1aa8af
Kosbys <kosmicabysslord@protonmail.com>
2025-11-24 15:34:10 +0200

    Make dock take entire screen

--- front/src/components/dock/Dock.tsx
+++ front/src/components/dock/Dock.tsx
@@ -6,7 +6,7 @@ import { LogoutButton } from "./LogoutButton";
 
 function Dock() {
     return (
-        <div className="fixed bottom-0 overflow-hidden self-center flex flex-row gap-4">
+        <div className="fixed bottom-0 overflow-hidden self-center justify-center items-center flex flex-row gap-4 w-full h-12 bg-primary">
             <HomeButton />
             <LogoutButton />
             <ThemeToggle />
