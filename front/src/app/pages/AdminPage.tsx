5df8a5794773f48c27a530e14edc00b3293a91f0
Kosbys <kosmicabysslord@protonmail.com>
2025-11-26 15:16:23 +0200

    Align admin page to center

--- front/src/app/pages/AdminPage.tsx
+++ front/src/app/pages/AdminPage.tsx
@@ -3,18 +3,18 @@ import { SectionList } from "@/components/section/SectionList";
 import { AddStation } from "@/components/station/AddStation";
 
 function AdminPage() {
-	return (
-		<div className="flex w-full h-full p-4 gap-4">
-			<div className="flex flex-col flex-1 items-start gap-4">
-				<AddSection />
-				<AddStation />
-			</div>
+    return (
+        <div className="flex flex-col items-center w-full h-full p-4 gap-4">
+            <div className="flex flex-col flex-1 items-start gap-4">
+                <AddSection />
+                <AddStation />
+            </div>
 
-			<div className="flex flex-col flex-2 items-start gap-4">
-				<SectionList />
-			</div>
-		</div>
-	);
+            <div className="flex flex-col flex-2 items-start gap-4">
+                <SectionList />
+            </div>
+        </div>
+    );
 }
 
 export { AdminPage };
